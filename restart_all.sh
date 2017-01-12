6b8c9acc31a0a05f1dcc2ed31ac80b92e5f1f5e5
Lubo Drobny <lubo.drobny@gmail.com>
2017-01-12 10:24:25 +0100

    9 nodes

--- restart_all.sh
+++ restart_all.sh
@@ -3,4 +3,9 @@ ssh 192.168.0.101 "sudo reboot" &
 ssh 192.168.0.102 "sudo reboot" &
 ssh 192.168.0.103 "sudo reboot" &
 ssh 192.168.0.104 "sudo reboot" &
+ssh 192.168.0.105 "sudo reboot" &
+ssh 192.168.0.106 "sudo reboot" &
+ssh 192.168.0.107 "sudo reboot" &
+ssh 192.168.0.108 "sudo reboot" &
+ssh 192.168.0.109 "sudo reboot" &
 
