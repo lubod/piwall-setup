c983458060289dc294e3b15345d06fcca10a8b3a
Lubo Drobny <lubo.drobny@gmail.com>
2017-01-12 10:23:31 +0100

    9 nodes

--- restart_slave_players.sh
+++ restart_slave_players.sh
@@ -7,4 +7,14 @@ ssh 192.168.0.103 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://2
 echo 103
 ssh 192.168.0.104 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
 echo 104
-
+#!/bin/sh
+ssh 192.168.0.105 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
+echo 105
+ssh 192.168.0.106 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
+echo 106
+ssh 192.168.0.107 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
+echo 107
+ssh 192.168.0.108 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
+echo 108
+ssh 192.168.0.109 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
+echo 109
