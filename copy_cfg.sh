0224ffc0bc548a7814ed7fec227548a494b209d0
Lubo Drobny <lubo.drobny@gmail.com>
2017-01-12 10:22:52 +0100

    9 nodes

--- copy_cfg.sh
+++ copy_cfg.sh
@@ -2,4 +2,8 @@ scp .piwall 192.168.0.101:
 scp .piwall 192.168.0.102:
 scp .piwall 192.168.0.103:
 scp .piwall 192.168.0.104:
-
+scp .piwall 192.168.0.105:
+scp .piwall 192.168.0.106:
+scp .piwall 192.168.0.107:
+scp .piwall 192.168.0.108:
+scp .piwall 192.168.0.109:
