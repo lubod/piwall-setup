6805b7cef3f8f8bd196ee3196b9fcee42dc1604b
Lubo Drobny <lubo.drobny@gmail.com>
2017-01-16 15:41:31 +0100

    clean up

--- restart_slave_players.sh
+++ restart_slave_players.sh
@@ -7,7 +7,6 @@ ssh 192.168.0.103 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://2
 echo 103
 ssh 192.168.0.104 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
 echo 104
-#!/bin/sh
 ssh 192.168.0.105 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
 echo 105
 ssh 192.168.0.106 "killall -9 pwomxplayer.bin; pwomxplayer --config=9bez udp://239.0.1.23:1234?buffer_size=1200000B" &
